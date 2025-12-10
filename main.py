483bfc291c03fde73a4050927edcba7b0caaec24
Durgesh-Dhore <dpdhore95@gmail.com>
2025-12-10 09:47:06 +0530

    Implement request hit counter and loggingadded Timestamp
    Added hit counter and logging for requests.

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from datetime import datetime
 
 app = FastAPI()
 
+hit_count = 0     # Track hit number
+
+
 @app.get("/")
-def read_root():
-    return{"message": "Hello world"}
+async def read_root(request: Request):
+    global hit_count
+    hit_count += 1  # Increase hit count for every request
+
+    # Get visitor IP
+    client_ip = request.client.host
+
+    # Get current timestamp
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Print in EC2 terminal only
+    print(f"Hit No: {hit_count} | IP: {client_ip} | Time: {timestamp}")
+
+    # Show only message to the user
+    return {"message": "Hello World"}
